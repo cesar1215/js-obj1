609fd30d9468087794a616b8b576adfcf20415da
cesar1215 <cenriquez@angloaleman.edu.mx>
2022-10-24 13:24:30 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+//Crea un objeto Escuela con las propiedades:
+//Salones: 3, alumnos: 50, maestros: ["luis", "Roberto", "Alberto"]
+const escuela = {
+    salones: 3,
+    alumnos: 50,
+    maestros: ["Luis","Roberto","Alberto"]
+}
+
+//Muestra en la consola el objeto escuela
+console.log(escuela);
+
+//Muestra en la consola la cantidad de alumnos del objeto escuela
+console.log(escuela.alumnos);
+
+//Muestra en consola al maestro Roberto del objeto escuela
+console.log(escuela.maestros[1]);
